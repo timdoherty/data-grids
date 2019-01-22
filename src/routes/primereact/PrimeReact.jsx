75d710e92b862b364c7231bef09287c4d300228b
Greg Burger <gregory.burger@procore.com>
2019-01-22 11:10:55 -0800

    Configure test table

--- src/routes/primereact/PrimeReact.jsx
+++ src/routes/primereact/PrimeReact.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+// import { ColumnGroup } from "primereact/columngroup";
+
 import Select from "@material-ui/core/Select";
 import Checkbox from "@material-ui/core/Checkbox";
 import MenuItem from "@material-ui/core/MenuItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Input from "@material-ui/core/Input";
 
-import data from "../../mocks/MOCK_DATA.js";
+import data_0000 from "../../mocks/MOCK_DATA.js";
+import data_1000 from "../../mocks/MOCK_DATA_1000.js";
+import data_2000 from "../../mocks/MOCK_DATA_2000.js";
+import data_3000 from "../../mocks/MOCK_DATA_3000.js";
+import data_4000 from "../../mocks/MOCK_DATA_4000.js";
+
+const data = data_0000
+  .concat(data_1000)
+  .concat(data_2000)
+  .concat(data_3000)
+  .concat(data_4000);
 
 class PrimeReact extends Component {
   constructor() {
     super();
+
     this.state = {
       rows: data,
       groupBy: ["car_make"],
@@ -19,14 +33,32 @@ class PrimeReact extends Component {
     };
   }
 
+  toTitle(key) {
+    return key
+      .split("_")
+      .map(word => {
+        return word.charAt(0).toUpperCase() + word.substr(1, word.length - 1);
+      })
+      .join(" ");
+  }
+
+  headerTemplate(data) {
+    return <div style={{ background: "#eee" }}>{data.car_make}</div>;
+  }
+
+  footerTemplate(data) {
+    return <div />;
+  }
+
   render() {
-    // TODO: format the value of key to use as a column header
-    let columnNames = Object.keys(data[0]).map(key => {
-      key: key;
-    });
+    let columns = Object.keys(data[0]).map(key => ({
+      key: key,
+      title: this.toTitle(key)
+    }));
 
     return (
       <div>
+        <p>Count:{this.state.rows.length}</p>
         <Select
           multiple
           value={this.state.groupBy}
@@ -42,8 +74,21 @@ class PrimeReact extends Component {
           ))}
         </Select>
 
-        <DataTable value={this.state.data}>
-          <Column field="x" header="X" />
+        <DataTable
+          value={this.state.rows}
+          rowGroupMode="subheader"
+          groupField="car_make"
+          rowGroupHeaderTemplate={this.headerTemplate}
+          rowGroupFooterTemplate={this.footerTemplate}
+        >
+          {columns.map(column => (
+            <Column
+              key={column.key}
+              field={column.key}
+              header={column.title}
+              sortable={true}
+            />
+          ))}
         </DataTable>
       </div>
     );
