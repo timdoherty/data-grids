ae149fd97f70212094c485518af326627c618db5
Tim Doherty <tim.doherty@procore.com>
2019-01-22 16:52:27 -0800

    Calling ag-grid a non-starter; too much work on top of a pricey license to justify going any further

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { LicenseManager } from 'ag-grid-enterprise';
+import '@procore/core-icons';
 
+LicenseManager.setLicenseKey(
+  'Evaluation_License-_Not_For_Production_Valid_Until_14_March_2019__MTU1MjUyMTYwMDAwMA==8917c155112df433b2b09086753e8903'
+);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
